5e8fdc711fc40873b8793b978888d3fc74119651
Tom <tquinlan1992@gmail.com>
2017-10-27 16:28:02 -0400

    use tquinlan-webpack, no webpack config

--- webpack-client/tquinlan1992.config.ts
+++
@@ -1,8 +0,0 @@
-import generatedWebpackConfig from './webpack.config';
-const webpack = require('webpack');
-
-const compiler = webpack(generatedWebpackConfig);
-
-compiler.run((err: any, stats: any) => {
-
-})

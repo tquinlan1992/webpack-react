cbf90f2bc80a0d60a168fb4c5517b051554af7bf
Thomas Quinlan <tquinlan1992@gmail.com>
2017-10-30 02:23:59 -0400

    update path of jest.config.js to use assetsTransformer of tquinlan1992-webpack-util

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     "json"
   ],
   "moduleNameMapper": {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jestUtil/assetsTransformer.js",
-    "\\.(css|less)$": "<rootDir>/jestUtil/assetsTransformer.js"
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/node_modules/tquinlan1992-webpack-util/src/jest/assetsTransformer.js",
+    "\\.(css|less)$": "<rootDir>/node_modules/tquinlan1992-webpack-util//src/jest/assetsTransformer.js"
   }
 }

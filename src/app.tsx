e9276926d38f9a43c10258a5bfbc0942e4a1056f
Tom <tquinlan1992@gmail.com>
2017-11-03 18:00:53 -0400

    set up routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import Main from './components/Greeter';
+//import Main from './components/Greeter';
 //import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './style/index.scss';
 import * as React from 'react';
@@ -7,20 +7,50 @@ import * as ReactDOM from 'react-dom';
 import AppBar from './components/AppBar';
 import Theme from './components/Theme';
 import getEnvrionmentConfigs from './getEnvrionmentConfigs';
+import {
+    BrowserRouter,
+    Route,
+    Link
+} from 'react-router-dom';
+
 getEnvrionmentConfigs.then((data: any) => {
     console.log("data", data);
 });
 
-ReactDOM.render(
+
+const App = () => (
     <Theme>
         <div>
             <AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" header="Header is working" headerChangeText="header changed">
+            
             </AppBar>
             <div>
                 <h1>Home Page </h1>
-                <Main />
             </div>
         </div>
-    </Theme>,
-    document.getElementById('app')
+    </Theme>
+);
+
+const test = (params: { match: any }): any => (
+    <div>
+        <Link to="/">Go home</Link>
+        <h1>
+            test
+        </h1>
+    </div>
+);
+const home = () => (
+    <h1>Home</h1>
+);
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <App/>
+            <Route exact path="" component={home} />
+            <Route path="/test" component={test} />
+        </div>
+    </BrowserRouter>
+    , document.getElementById('app')
 );

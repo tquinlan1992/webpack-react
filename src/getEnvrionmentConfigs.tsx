e9caabdf7e8ae30a94db11dc44b7c7acd05d2631
Tom <tquinlan1992@gmail.com>
2017-11-03 10:43:24 -0400

    fix tsconfig src

--- src/getEnvrionmentConfigs.tsx
+++ src/getEnvrionmentConfigs.tsx
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
 const getConfigsPromise = new Promise((resolve, reject) => {
-    axios.get(`/environmentConfigs/environment.json`)
+    axios.get(`/environment.json`)
         .then((res: any) => {
             console.log("res", res);
             resolve(res.data);
         });
 });
+
+getConfigsPromise.then((res:any) => {
+    axios.get(res.api + "/test.json")
+        .then((res: any) => {
+            console.log("api test data", res.data);
+        });
+})
 
 export default getConfigsPromise;

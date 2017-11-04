d0ba7fc1ae7449e7029787327a15f095fd953b36
Thomas Quinlan <tquinlan1992@gmail.com>
2017-11-03 20:07:13 -0400

    add semicolon

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,4 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/node_modules/tquinlan1992-webpack-util/src/jest/assetsTransformer.js",
     "\\.(css|less)$": "<rootDir>/node_modules/tquinlan1992-webpack-util/src/jest/assetsTransformer.js"
   }
-}
+};
